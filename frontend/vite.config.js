139376648beb4409c151689430ebe866c865d5df
MollyJosefin <molly.arnaryd@hotmail.com>
2025-05-27 12:03:16 +0200

    Update vite.config.js
    uppdaterar för tesntingen

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/sanity-react-cms-app/",
+  base: '/sanity-react-cms-app/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: [],
+  }
 });
